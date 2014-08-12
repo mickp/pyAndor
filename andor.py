0c559cee7dc47d9c041770115477f372a4c37a99
Mick Phillips <mick.phillips@gmail.com>
2014-08-12 18:37:10 +0100

    Imported SDK func as Camera instance methods.

--- andor.py
+++ andor.py
@@ -1,45 +1,65 @@
 import andorsdk as sdk
 import threading
-from ctypes import byref, c_float, c_int, c_long
-
-# do not edit! added by PythonBreakpoints
-from pdb import set_trace as _breakpoint
-
+from ctypes import byref, c_float, c_int, c_long, c_ulong
+import functools
 
 dll_lock = threading.Lock()
 
-def with_lock(func):
-        def wrapper(self, *args, **kwargs):
-            _breakpoint()  # d35fe60d
-            had_lock_on_entry = self.has_lock
-            
-            try:
-                if not had_lock_on_entry:
-                    dll_lock.acquire()
-                    sdk.SetCurrentCamera(self.handle)
-                    self.has_lock = True
-                    print "lock acquired, camera set"
-                    result = func(self, *args, **kwargs)
-                    dll_lock.release()
-                    self.has_lock = False
-                    print "lock released"
-                else:
-                    result = func(self, *args, **kwargs)
-                return result
-            except:
+def with_camera(func):
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):   
+        had_lock_on_entry = self.has_lock
+        try:
+            if not had_lock_on_entry:
+                dll_lock.acquire()
+                self.has_lock = True
+                sdk.SetCurrentCamera(self.handle)
+                result = func(self, *args, **kwargs)
                 dll_lock.release()
                 self.has_lock = False
-                raise
+            else: # already had lock
+                result = func(self, *args, **kwargs)
+            return result
+        except:
+            if dll_lock.locked():  
+                dll_lock.release()
+            self.has_lock = False
+            raise
+    return wrapper
 
-        return wrapper
+
+def sdk_call(func):
+    ## Simply passes args to func, without self.
+    @functools.wraps(func)
+    def sdk_wrapper(self, *args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            raise
+    return sdk_wrapper
+
+
+class CameraMeta(type):
+    """Metaclass that adds DLL methods to the Camera class.
+
+    Methods from the SDK DLL are wrapped by 'sdk_call' to remove 'self' from
+    the args, then by 'with_camera' so that
+    * a lock is obtained on the DLL;
+    * the DLL is set to act on the Camera instance;
+    * the DLL method is called;
+    * the lock is released."""
+    def __new__(meta, classname, supers, classdict):
+        for f in sdk.camerafuncs:
+            classdict[f.__name__] = with_camera(sdk_call(f))
+        return type.__new__(meta, classname, supers, classdict)
 
 
 class Camera(object):
     """Camera class for Andor cameras.
     
-    All instance methods should be decorated with @with_lock: this obtains a
+    All instance methods should be decorated with @with_camera: this obtains a
     lock on the DLL, and sets the target camera for DLL methods."""
-
+    __metaclass__ = CameraMeta
     # Map handle values to camera instances
     handle_to_camera = {}
     # A list of camera instances
@@ -60,7 +80,6 @@ class Camera(object):
         for i in range(num_cameras.value):
             handle = c_long()
             sdk.GetCameraHandle(i, byref(handle))
-
             cameras.append(Camera(handle))
             handle_to_camera.update({handle.value: i})
 
@@ -74,87 +93,46 @@ class Camera(object):
         self.vs_speed = None
 
 
-    @with_lock
-    def cooler_on(self):
-        sdk.CoolerON()
+    def prepare(self):
+        self.get_detector()
+        self.GetCapabilities(self.caps)
 
 
-    @with_lock
-    def cooler_off(self):
-        sdk.CoolerOFF()
 
-
-    @with_lock
-    def initialize(self):
-        return sdk.Initialize('') == sdk.DRV_SUCCESS:
-
-
-    @with_lock
-    def get_capabilities(self):
-        sdk.GetCapabilities(byref(self.caps))
-
-
-    @with_lock
+    @with_camera
     def get_detector(self):
         nx, ny = c_int(), c_int()
-        sdk.GetDetector(byref(nx), byref(ny))
+        self.GetDetector(byref(nx), byref(ny))
         self.nx = nx.value
         self.ny = ny.value
         return (self.nx, self.ny)
 
 
-    @with_lock
+    @with_camera
     def get_hardware_version(self):
-        pcb, decode, dummy1, dummy2, version, build = 6 * [c_int()]
+        pcb, decode, dummy1, dummy2, version, build = 6 * [c_ulong()]
         plist = [pcb, decode, dummy1, dummy2, version, build]
         parameters = [byref(p) for p in plist]
-        sdk.GetHardwareVersion(*parameters)
+        self.GetHardwareVersion(*parameters)
         result = [p.value for p in plist]
         return result
 
 
-    @with_lock
+    @with_camera
     def get_fastest_recommended_vs_speed(self):
         index = c_int()
         speed = c_float()
-        sdk.GetFastestRecommendedVSSpeed(byref(index), byref(speed))
+        self.GetFastestRecommendedVSSpeed(index, speed)
         self.vs_speed = speed.value
-        return speed.value
+        return (index.value, speed.value)
 
 
-    @with_lock
+    @with_camera
     def is_preamp_gain_available(self, channel, amplifier, index, gain):
         status = c_int()
         sdk.IsPreAmpGainAvailable(int(channel), 
                                   int(amplifier), 
                                   int(index), 
                                   int(gain),
-                                  byref(status))
+                                  status)
         return status.value
-
-
-    @with_lock
-    def set_dma_parameters(self, max_images_per_dma, seconds_per_dma):
-        sdk.SetDMAParameters(int(max_images_per_dma),
-                             float(seconds_per_dma))
-
-
-    @with_lock
-    def set_em_gain_mode(sefl, mode):
-        sdk.SetEMGainMode(int(mode))
-
-
-    @with_lock
-    def set_fan_mode(self, mode):
-        sdk.SetFanMode(int(mode))
-
-
-    @with_lock
-    def set_preamp_gain(self, index):
-        sdk.SetPreAmpGain(int(index))
-
-
-    @with_lock
-    def set_read_mode(self, n):
-        if n != self.read_mode:
-            sdk.SetReadMode(int(n))
