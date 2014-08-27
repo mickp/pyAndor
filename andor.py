c301d36680fc068df91558330c1afc416df30c0b
Mick Phillips <mick.phillips@gmail.com>
2014-08-27 18:44:25 +0100

    Added camera setup and disable methods.

--- andor.py
+++ andor.py
@@ -1,6 +1,7 @@
 """A module for control of Andor cameras via the Andor SDK.
 
-This module defines the Camera class for Andor cameras."""
+This module defines the Camera class for Andor cameras.
+"""
 import andorsdk as sdk
 import functools
 import Pyro4
@@ -38,7 +39,8 @@ def with_camera(func):
 
     If there are multiple cameras per process, this decorator obtains a lock
     on the DLL and calls SetCurrentCamera to ensure that the library acts on 
-    the correct piece of hardware."""
+    the correct piece of hardware.
+    """
     @functools.wraps(func)
     def wrapper(self, *args, **kwargs):   
         if not self.singleton:
@@ -70,7 +72,8 @@ def with_camera(func):
 def sdk_call(func):
     """A decorator for DLL functions called as methods of Camera.
 
-    This decorator simply passes args to func, without self."""
+    This decorator simply passes args to func, without self.
+    """
     @functools.wraps(func)
     def sdk_wrapper(self, *args, **kwargs):
         try:
@@ -85,7 +88,8 @@ class CameraManager(object):
 
     This used to be handled by class variables and class methods, but
     that approach will not work when we need to spread several cameras
-    over separate processes."""
+    over separate processes.
+    """
     def __init__(self):
         # Map handle values to camera instances
         self.handle_to_camera = {}
@@ -99,7 +103,8 @@ class CameraManager(object):
         """Search for cameras and create Camera instances.
 
         Camera instances are tracked in the class variables
-        Camera.cameralist and Camera.handle_to_camera."""
+        Camera.cameralist and Camera.handle_to_camera.
+        """
         for cam in self.cameras:
             self.cameras.remove(cam)
 
@@ -133,6 +138,7 @@ class CameraServer(Process):
     def __init__(self, index):
         super(CameraServer, self).__init__()
         self.index = index
+        #TODO - should read serial_to_* from config file.
         self.serial_to_host = {9145: '127.0.0.1', 9146: '127.0.0.1'}
         self.serial_to_port = {9145: 7776, 9146: 7777}
         self.cam = None
@@ -191,7 +197,8 @@ class CameraMeta(type):
     * a lock is obtained on the DLL;
     * the DLL is set to act on the Camera instance;
     * the DLL method is called;
-    * the lock is released."""
+    * the lock is released.
+    """
     def __new__(meta, classname, supers, classdict):
         for f in sdk.camerafuncs:
             classdict[f.__name__] = with_camera(sdk_call(f))
@@ -202,7 +209,8 @@ class Camera(object):
     """Camera class for Andor cameras.
     
     All instance methods should be decorated with @with_camera: this obtains a
-    lock on the DLL, and sets the target camera for DLL methods."""
+    lock on the DLL, and sets the target camera for DLL methods.
+    """
 
     # Use the CameraMeta class to add DLL methods to this class.
     __metaclass__ = CameraMeta
@@ -210,13 +218,29 @@ class Camera(object):
 
     def __init__(self, handle, singleton=False):
         """Init a Camera instance for hardware with ID=handle."""
+        # SDK's handle for the camera
         self.handle = handle
+        # Does this camera have the lock on the DLL?
         self.has_lock = False
+        # Detector dimensions in pixels.
         self.nx, self.ny = None, None
+        # Detector capabilties.
         self.caps = sdk.AndorCapabilities()
+        # Readout mode.
         self.read_mode = None
+        # Vertical shift speed.
         self.vs_speed = None
+        # Is this the only camera in this process?
         self.singleton = singleton
+        # Is the camera enabled.
+        self.enabled = False
+        # Temperature min. and max. possible values.
+        self.t_min = c_int()
+        self.t_max = c_int()
+        # Temperature set point and threshold.
+        self.t_set_point = c_int()
+        self.t_threshold = 2
+        self.t_state = None
 
 
     @with_camera
@@ -225,10 +249,48 @@ class Camera(object):
 
 
     @with_camera
-    def prepare(self):
-        """Prepare the camera for data acquisition."""
+    def disable(self):
+        self.enabled = False
+        try:
+            self.abort()
+        except:
+            pass
+        self.make_safe()
+        # Could call self.Shutdown here, but that would require slow
+        # AD calibration on next enable.
+        # TODO: figure out where to call ShutDown before exit / on
+        # camera destruction ... can't see how to use a 'with' context
+        # manager here.
+
+
+    @with_camera
+    def enable(self, settings):
+        # Clear the flag so that our client will poll until it is True.
+        self.enabled = False
+        try:
+            # Stop whatever the camera was doing.
+            self.abort()
+        except Exception:
+            try:
+                self.Initialize('')
+            except e:
+                raise
         self.get_detector()
         self.get_capabilities()
+        if settings.get('isWaterCooled'):
+            # Use fan at low speed.
+            self.SetFanMode(1)
+        else:
+            # Use fan at full speed.
+            self.SetFanMode(0)
+        self.GetTemperatureRange(self.temperature_min, self.temperature_max)
+        t_target = int(settings.get('targetTemperature', 0))
+        self.t_set_point.value = max(self.t_min.value,
+                                        min(self.t_max.value, t_target))
+        self.SetTemperature(self.t_set_point)
+        self.CoolerON()
+        self.enabled = True
+        return self.enabled
 
 
     @with_camera
@@ -237,7 +299,7 @@ class Camera(object):
         accumulate = c_float()
         kinetic = c_float()
         sdk.GetAcquisitionTimings(exposure, accumulate, kinetic)
-        return (exposure, accumulate, kinetic)
+        return (exposure.value, accumulate.value, kinetic.value)
 
 
     @with_camera
@@ -315,6 +377,13 @@ class Camera(object):
 
 
     @with_camera
+    def get_temperature(self):
+        temperature = c_int()
+        self.t_state = sdk.GetTemperature(temperature)
+        return temperature.value
+
+
+    @with_camera
     def get_hardware_version(self):
         pcb, decode, dummy1, dummy2, version, build = 6 * [c_ulong()]
         plist = [pcb, decode, dummy1, dummy2, version, build]
@@ -322,6 +391,20 @@ class Camera(object):
         sdk.GetHardwareVersion(*parameters)
         result = [p.value for p in plist]
         return result
+
+
+    @with_camera
+    def make_safe(self):
+        # Set lowest EM gain on EMCCD cameras.
+        if self.caps.ulSetFunctions & sdk.AC_SETFUNCTION_EMCCDGAIN:
+            self.SetEMCCDGain(0)
+
+        # Switch to conventional amplifier on supported cameras.
+        if self.caps.ulCameraType in [sdk.AC_CAMERATYPE_CLARA,
+                                      sdk.AC_CAMERATYPE_IXON,
+                                      sdk.AC_CAMERATYPE_IXONULTRA,
+                                      sdk.AC_CAMERATYPE_NEWTON]:
+            self.SetOutputAmplifier(1)
 
 
     @with_camera
@@ -333,3 +416,33 @@ class Camera(object):
                                   int(gain),
                                   status)
         return status.value
+
+
+    @with_camera
+    def is_ready(self):
+        temperature = int(self.get_temperature())
+        t_on_target = abs(self.t_set_point - temperature) < self.t_threshold
+        return self.enabled and t_on_target
+
+
+    @with_camera
+    def prepare(self):
+        """Prepare the camera for data acquisition."""
+        self.get_detector()
+        self.get_capabilities()
+
+
+    @with_camera
+    def set_exposure_time(self, exposure_time):
+        """Set the exposure time and update vertical shift speed."""
+        self.SetExposureTime(float(exposure_time))
+        set_fastest_vs_speed()
+        exposure, accumulate, kinetic = self.get_acquisition_timings()
+        return exposure
+
+
+    def set_fastest_vs_speed(self):
+        """Update the vertical shift speed to fasted recommended speed."""
+        (index, speed) = self.get_fastest_recommended_vs_speed
+        self.SetVSSpeed(int(index))
+        return speed
